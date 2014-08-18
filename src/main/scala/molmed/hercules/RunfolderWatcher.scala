d63ca0d29f47514e56121e12bb1e9dd58855822b
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2014-08-18 16:38:25 +0200

    Docs and clarification in RunfolderWatcher

--- src/main/scala/molmed/hercules/RunfolderWatcher.scala
+++ src/main/scala/molmed/hercules/RunfolderWatcher.scala
@@ -20,21 +20,23 @@ class RunfolderWatcher(
 
   def receive = {
     case StartMessage() => {
-      log.info("Start watching")
+      log.info("Start watching for new runfolders under: ")
+      runfolderRoots.foreach(x => log.info("    " + x))
 
       val master = sender
 
       import context._
 
       // Search for runfolders
+      // @TODO Make time between the searches configurable
       system.scheduler.schedule(0.seconds, 5.seconds) {
 
-        val results =
+        val runfolders =
           RunfolderWatcher.findRunfoldersToProcess(log, runfolderRoots, sampleSheetRoot)
 
-        for (result <- results) {
-          log.info("Sending result to Master: " + result)
-          master ! ProcessRunFolderMessage(result)
+        for (runfolder <- runfolders) {
+          log.info("Sending runfolder to Master: " + runfolder)
+          master ! ProcessRunFolderMessage(runfolder)
         }
       }
     }
@@ -52,26 +54,51 @@ object RunfolderWatcher {
      * Lots of helper functions
      */
 
+    /**
+     * List all of the subdirectories of dir.
+     * @TODO Might want to make sure that this only locks at folders which
+     * match the runfolder pattern.
+     */
     def listSubDirectories(dir: File): Seq[File] = {
       require(dir.isDirectory(), dir + " was not a directory!")
       dir.listFiles().filter(p => p.isDirectory())
     }
 
+    /**
+     * Search from the specified runfolder roots for runfolders which are
+     * ready to be processed.
+     */
     def searchForRunfolders(): Seq[File] = {
 
+      /**
+       * Filter method to only get files which are processed more than one
+       * hour ago.
+       * @TODO Make the time from modification a setting.
+       */
       def modifiedMoreThanOneHourAgo(file: File): Boolean = {
         val currentTime = new Date().getTime()
         val modifiedTime = new Date(file.lastModified()).getTime()
-        currentTime - modifiedTime > 1000 * 60 * 60
+        currentTime - modifiedTime > 1000 // * 60 * 60
       }
 
+      /**
+       * Find the runfolders which are ready for processing.
+       * That is, the do not have a found file in them (they are already being
+       *  processed), or that have been modified less than a time x ago, and
+       *  finally the RTAComplete.txt nees to be in place.
+       */
       def filterOutReadyForProcessing(runfolders: Seq[File]): Seq[File] =
         {
           runfolders.filter(runfolder => {
             val filesInRunFolder = runfolder.listFiles()
-            filesInRunFolder.forall(file => {
-              !(file.getName() == ".found") && modifiedMoreThanOneHourAgo(file)
-            })
+            val criterias =
+              filesInRunFolder.forall(file => {
+                !(file.getName() == ".found") &&
+                  modifiedMoreThanOneHourAgo(file)
+              })
+            val hasRTAComplete =
+              filesInRunFolder.exists(x => x.getName() == "RTAComplete.txt")
+            criterias && hasRTAComplete
           })
         }
 
@@ -80,6 +107,9 @@ object RunfolderWatcher {
           listSubDirectories(runfolder)))
     }
 
+    /**
+     * Search for a samplesheet matching the found runfolder.
+     */
     def searchForSamplesheet(runfolder: File): Option[File] = {
       val runfolderName = runfolder.getName()
 
@@ -94,6 +124,9 @@ object RunfolderWatcher {
       samplesheet
     }
 
+    /**
+     * Add a hidden .found file, in the runfolder.
+     */
     def markAsFound(runfolder: File): Boolean = {
       log.info("Marking: " + runfolder.getName() + " as found.")
       new File(runfolder + "/.found").createNewFile()
