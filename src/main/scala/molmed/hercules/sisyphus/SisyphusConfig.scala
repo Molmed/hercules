b1261358f6da2259bfc1c61c51b0d56e89e09675
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2014-08-18 16:27:38 +0200

    Adding some temporary paths to Sisyphus

--- src/main/scala/molmed/hercules/sisyphus/SisyphusConfig.scala
+++ src/main/scala/molmed/hercules/sisyphus/SisyphusConfig.scala
@@ -1,15 +1,20 @@
 package molmed.hercules.sisyphus
 
+import java.io.File
+
 trait SisyphusConfig {
 
   val currentSisyphusVersion = "v14.1.2"
 
+  //@TODO Make this configurable!
   object BiotankSisyphusConfig {
-    val sisyphusInstallLocation = "???"
+    val sisyphusInstallLocation: File = new File("/vagrant/sisyphus/")
+    val sisyphusLogLocation: File = new File("/vagrant/sisyphus/")
   }
 
   object UppmaxSisyphusConfig {
-    val sisyphusInstallLocation = "???"
+    val sisyphusInstallLocation: File = ???
+    val sisyphusLogLocation: File = ???
   }
 
 }
