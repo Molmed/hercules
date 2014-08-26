f05ccc5af787ba4439f75d1a21728fd92ce209d6
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2014-08-26 16:21:51 +0200

    Embryo for downloading testdata

---
+++ download_test_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+# Will download the test data compiled on mm-cluster 
+mkdir -p test_data
+rsync -r --progress mm-cluster:/data/hercules_test_data test_data/
