654acf893773b6769368188d3fcd9e601ffc67d8
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2014-09-01 17:00:24 +0200

    Out with the old! (Removing the old prototype)

--- src/main/scala/molmed/hercules/processes/biotank/WriteTestFileToDirectoryProcess.scala
+++
@@ -1,8 +0,0 @@
-package molmed.hercules.processes.biotank
-
-import molmed.hercules.Runfolder
-
-case class WriteTestFileToDirectoryProcess(runfolder: Runfolder)
-    extends BiotankProcess {
-  val command = "hostname > " + runfolder.runfolder + "/testfile"
-}
