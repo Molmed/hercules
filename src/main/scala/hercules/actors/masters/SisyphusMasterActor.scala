a6e76d48a616c963aabc8147295e1e5ac7f3a102
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2014-09-02 16:01:57 +0200

    Added companion objects to actors
    This is the recommended best practice way to create the actors, see:
    http://doc.akka.io/docs/akka/snapshot/scala/actors.html - the
    "Recommended Practices" section.

--- src/main/scala/hercules/actors/masters/SisyphusMasterActor.scala
+++ src/main/scala/hercules/actors/masters/SisyphusMasterActor.scala
@@ -1,6 +1,11 @@
 package hercules.actors.masters
 
 import akka.actor.ActorContext
+import akka.actor.Props
+
+object SisyphusMasterActor {
+  def props(): Props = Props(new SisyphusMasterActor())
+}
 
 /**
  * Defines the logic for running the Sisyphus workflow
--- src/main/scala/hercules/actors/processingunitwatcher/IlluminaProcessingUnitWatcherActor.scala
+++ src/main/scala/hercules/actors/processingunitwatcher/IlluminaProcessingUnitWatcherActor.scala
@@ -1,14 +1,23 @@
 package hercules.actors.processingunitwatcher
 
 import hercules.config.processing.ProcessingUnitWatcherConfig
+import akka.actor.Props
+import hercules.config.processing.ProcessingUnitWatcherConfig
+import hercules.entities.ProcessingUnit
+
+object IlluminaProcessingUnitWatcherActor {
+  def props(config: ProcessingUnitWatcherConfig): Props =
+    Props(new IlluminaProcessingUnitWatcherActor(config))
+}
 
 /**
  * Base class for Actors which are watching for finished illumina runfolders.
- * //@TODO I'm not so sure that this class should really be abstact.
- *         Maybe we should provide the concrete implementation in here?
  */
-abstract class IlluminaProcessingUnitWatcherActor(
-  config: ProcessingUnitWatcherConfig)
+class IlluminaProcessingUnitWatcherActor(
+  val config: ProcessingUnitWatcherConfig)
     extends ProcessingUnitWatcherActor {
 
+ def receive = ??? 
+ def isDone(unit: ProcessingUnit): Boolean = ???
+  
 }
