0c7fe2d7bb8fa1d8604c21b1596f601f0413687b
Pontus Larsson <Pontus@molmed-98.medsci.uu.se>
2014-09-12 17:50:25 +0200

    made NotificationUnit a trait

--- src/main/scala/hercules/entities/notification/NotificationUnit.scala
+++ src/main/scala/hercules/entities/notification/NotificationUnit.scala
@@ -4,4 +4,4 @@ package hercules.entities.notification
  * Provides a base for representing a notification. Subclasses extends this and
  * implements the means of delivery
  */
-abstract class NotificationUnit() {}
+trait NotificationUnit {}
