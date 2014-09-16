5a2cf3a455429480f516442d5d0a91f329d8cf38
Pontus Larsson <Pontus@molmed-98.medsci.uu.se>
2014-09-16 10:12:19 +0200

    moved default email settings to config

--- src/main/scala/hercules/config/notification/EmailNotificationConfig.scala
+++ src/main/scala/hercules/config/notification/EmailNotificationConfig.scala
@@ -7,26 +7,10 @@ import java.util.List
 
 object EmailNotificationConfig {
 
-  /** 
-    * Return a ConfigFactory.Config object with default email settings which can be 
-    * overridden with settings from the config file
-  */
-  def defaults(): Config = {
-    val defaultSettings = new java.util.Hashtable[String,Object]()
-    //defaultSettings.put("email.recipients",new java.util.ArrayList[String]().subList(0,0))
-    defaultSettings.put("smtp_host","localhost")
-    defaultSettings.put("smtp_port",new java.lang.Integer(25))
-    defaultSettings.put("sender",this.getClass.getName + "@" + java.net.InetAddress.getLocalHost.getHostName)
-    defaultSettings.put("prefix","[Hercules]")
-    ConfigFactory.parseMap(defaultSettings,"default email settings")
-  }
-  
   /** Create and return a EmailNotificationConfig from the supplied 
    *  configuration. If necessary, provide default values for missing options.
    */
-  def getEmailNotificationConfig(emailConf: Config): EmailNotificationConfig = {
-    val conf = emailConf.withFallback(defaults)
-    println(conf.root.render)
+  def getEmailNotificationConfig(conf: Config): EmailNotificationConfig = {
     val emailRecipients = conf.getStringList("recipients")  
     val emailSender = conf.getString("sender")
     val emailSMTPHost = conf.getString("smtp_host")
