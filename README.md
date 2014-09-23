232482eb1f1840821960f1c57b7cdcae1b1e7366
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2014-09-23 17:40:01 +0200

    Added packaging capabilities
    This includes setting up a simple logger which will write under
    /var/log/hercules.log at the moment. Later we might want to hook this up
    to logstash or at least make it a rotating log.
    
    I also configured the app main entry to by default try to get the roles
    which it should start up (master, watcher, ...) from the
    application.conf. That way one or more roles that we want running on the
    same machine can be started up together.
    
    Also changed a returns -> return in the documentation (because that is the correct tag to
    use).

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Hercules
 ========
 
+*NOTE:* This this project is under heavy development and anything can change without previous notice (and a lot of the functionality is not in place yet).
+
 The amazing Hercules project, taking the stone out of Sisyphus hands! (A real project description should be inserted here at some point)
 
 Prepare for development
@@ -23,4 +25,31 @@ And to ssh into them:
     vagrant ssh <machine name>
     
 You can also download a minimal test data set (provided that you have access to the correct ssh config) by running the `download_test_data.sh` script.
+
+Install instructions (extremely preliminary)
+--------------------------------------------
+To package Hercules as a rpm:
+
+    sbt rpm:packageBin
+
+And then install the rpm with
+
+    sudo yum install target/rpm/RPMS/noarch/hercules-<version>.noarch.rpm
+
+This should create all the necesary upstart scripts so that `service hercules <command>` can be used to control the server.
+
+For debian based systems this is slightly more complicated:
+
+   sbt debian:packageBin
+
+And then:
+
+    dpkg -i target/hercules-<version>.deb
+
+to install the debian file. Unfortuneatly the Upstart system scripts does not seem to work, so SystemV needs to be used to start the service. So use:
+
+    sudo /etc/init.d/hercules <command>
+
+To control the service.
+
 
--- src/main/scala/hercules/Hercules.scala
+++ src/main/scala/hercules/Hercules.scala
@@ -5,6 +5,12 @@ import hercules.actors.masters.SisyphusMasterActor
 import hercules.actors.processingunitwatcher.IlluminaProcessingUnitWatcherActor
 import hercules.actors.interactive.InteractiveActor
 import hercules.actors.notifiers.EmailNotifierActor
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import akka.dispatch.Foreach
+import scala.collection.JavaConversions._
+import akka.event.Logging
+import org.slf4j.LoggerFactory
 
 /**
  * The main entry point for the application
@@ -12,71 +18,103 @@ import hercules.actors.notifiers.EmailNotifierActor
  * Will parse the command line options and initiate the appropriate
  * system depending on the command and options passed.
  */
-object Hercules extends App {
+object Hercules extends App { 
 
+  val log = LoggerFactory.getLogger("Hercules")
+  
   // A very simple command line parser that is able to check if this is a
   // worker or a master that is starting up.
-  sealed trait Command
-  case object RunMaster extends Command
-  case object RunDemultiplexter extends Command
-  case object RunRunfolderWatcher extends Command
-  case object RunInteractive extends Command
-  case object RunNotifier extends Command
+  sealed trait Role
+  case object RunMaster extends Role
+  case object RunDemultiplexer extends Role
+  case object RunRunfolderWatcher extends Role
+  case object RunInteractive extends Role
+  case object RunNotifier extends Role
+  case object RunHelp extends Role
+
+  def string2Role(s: String): Role = {
+    s match {
+      case "master"        => RunMaster
+      case "demultiplexer" => RunDemultiplexer
+      case "watcher"       => RunRunfolderWatcher
+    }
+  }
 
   case class CommandLineOptions(
-    applicationType: Option[Command] = None,
+    applicationType: Option[List[Role]] = None,
     command: Option[String] = None,
     unitName: Option[String] = None)
 
   val parser = new scopt.OptionParser[CommandLineOptions]("Hercules") {
 
+    cmd("help") action { (_, c) =>
+      c.copy(applicationType = Some(List(RunHelp)))
+    }
+
     cmd("master") action { (_, c) =>
-      c.copy(applicationType = Some(RunMaster))
+      c.copy(applicationType = Some(List(RunMaster)))
     }
 
     cmd("demultiplexer") action { (_, c) =>
-      c.copy(applicationType = Some(RunDemultiplexter))
-    }
-
-    cmd("notifier") action { (_, c) =>
-      c.copy(applicationType = Some(RunNotifier))
+      c.copy(applicationType = Some(List(RunDemultiplexer)))
     }
 
     cmd("watcher") action { (_, c) =>
-      c.copy(applicationType = Some(RunRunfolderWatcher))
+      c.copy(applicationType = Some(List(RunRunfolderWatcher)))
     }
+    //@TODO Temporarily removed this as I think that maybe we want some
+    // other way to do this after we fix the REST API. /JD 22/9 2014
+    //    cmd("interactive") action { (_, c) =>
+    //      c.copy(applicationType = Some(List(RunInteractive)))
+    //    } children (
+    //      cmd("restart") action { (_, c) => c.copy(command = Some("restart")) } children (
+    //        arg[String]("unit") required () action { (x, c) =>
+    //          c.copy(unitName = Some(x))
+    //        }))
 
-    cmd("interactive") action { (_, c) =>
-      c.copy(applicationType = Some(RunInteractive))
-    } children (
-      cmd("restart") action{(_, c) => c.copy(command = Some("restart"))} children (
-        arg[String]("unit") required () action { (x, c) =>
-          c.copy(unitName = Some(x))
-        }))
+  }
+
+  def parseCommandLineOptions(config: CommandLineOptions): Unit = {
+
+    config.applicationType match {
+      // The default case is to try to get the roles from the application.conf
+      case None => {
+        val configLoader = ConfigFactory.load()
+        val rolesToStart = configLoader.getStringList("hercules.roles").toList
         
-    cmd("notifier") action { (_, c) =>
-      c.copy(applicationType = Some(RunNotifier))
+        log.info(
+          "When Hercules starts without any arguments it will by default " +
+            "read the list roles from the application.conf. Will now attempt to " + 
+            "start the following roles: ")
+        rolesToStart.foreach(log.info(_))
+        
+        val mappedToRoles = rolesToStart.map(x => string2Role(x))
+        val updatedConfig = config.copy(applicationType = Some(mappedToRoles))
+        parseCommandLineOptions(updatedConfig)
+      }
+      case Some(list) => {
+        list.foreach { x =>
+          x match {
+            case RunHelp =>
+              parser.showUsage
+            case RunMaster =>
+              SisyphusMasterActor.startSisyphusMasterActor()
+            case RunNotifier =>
+              EmailNotifierActor.startEmailNotifierActor()
+            case RunDemultiplexer =>
+              IlluminaDemultiplexingActor.startIlluminaDemultiplexingActor()
+            case RunRunfolderWatcher =>
+              IlluminaProcessingUnitWatcherActor.startIlluminaProcessingUnitWatcherActor()
+            case RunInteractive =>
+              InteractiveActor.startInteractive(config.command.get, config.unitName.get)
+          }
+        }
+      }
     }
-    
   }
 
   parser.parse(args, CommandLineOptions()) map { config =>
     // do stuff
-    config.applicationType match {
-      case Some(RunMaster) =>
-        SisyphusMasterActor.startSisyphusMasterActor()
-      case Some(RunNotifier) => 
-        EmailNotifierActor.startEmailNotifierActor()
-      case Some(RunDemultiplexter) =>
-        IlluminaDemultiplexingActor.startIlluminaDemultiplexingActor()
-      case Some(RunRunfolderWatcher) =>
-        IlluminaProcessingUnitWatcherActor.startIlluminaProcessingUnitWatcherActor()
-      case Some(RunInteractive) =>
-        InteractiveActor.startInteractive(config.command.get, config.unitName.get)
-      case None => parser.showUsageAsError
-    }
-  } getOrElse {
-    // arguments are bad, error message will have been displayed
+    parseCommandLineOptions(config)
   }
-
 }
